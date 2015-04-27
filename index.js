d3f33a6534fc21895ca4c00c4ccc24be5429cb3e
Georg Schmidl <georg.schmidl@gmail.com>
2015-04-27 22:27:51 +0200

    throw error if input is malformed

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 module.exports = function(events) {
 
+  for (var i = 0; i < events.length; i++) {
+    if (typeof events[i] != 'object') { throw new Error('Event ' + i + ' is not an object.'); }
+    if (typeof events[i].name != 'string') { throw new Error('Name of event ' + i + ' is not a string.'); }
+    if (!(events[i].date instanceof Date)) { throw new Error('Date of event ' + i + ' is not a Date object.'); }
+  }
+
   return events;
 
 };
--- test/index.js
+++ test/index.js
@@ -11,4 +11,24 @@ describe('uniqueEventNames', function() {
     expect(events).to.have.length(0);
   });
 
+  it('should throw error if event is not an object', function(){
+    var events = ['test'];
+
+    expect(function() { uniqueEventNames(events); }).to.throw('Event 0 is not an object.');
+  });
+
+  it('should throw error if event name is not a string', function(){
+    var events = [{}];
+
+    expect(function() { uniqueEventNames(events); }).to.throw('Name of event 0 is not a string.');
+  });
+
+  it('should throw error if event date is not a Date object', function(){
+    var events = [{
+      name: 'test'
+    }];
+
+    expect(function() { uniqueEventNames(events); }).to.throw('Date of event 0 is not a Date object.');
+  });
+
 });
