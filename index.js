4e3f9cba7d77fe0412919a5d9b81f0743d4e4d40
Georg Schmidl <georg.schmidl@gmail.com>
2015-05-02 13:16:22 +0200

    generate ymn

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
 
-module.exports = function(events) {
+function generateYmn(events) {
+  var ymnTrees = buildYmnTrees(events);
+
+  for (var name in ymnTrees) {
+    traverseYmnTree(ymnTrees[name]);
+  }
+  return events;
+}
+
+function buildYmnTrees(events) {
+  var ymnTrees = {};
 
   for (var i = 0; i < events.length; i++) {
-    if (typeof events[i] != 'object') { throw new Error('Event ' + i + ' is not an object.'); }
-    if (typeof events[i].name != 'string') { throw new Error('Name of event ' + i + ' is not a string.'); }
-    if (!(events[i].date instanceof Date)) { throw new Error('Date of event ' + i + ' is not a Date object.'); }
+    var event = events[i];
+
+    if (typeof event != 'object') { throw new Error('Event ' + i + ' is not an object.'); }
+    if (typeof event.name != 'string') { throw new Error('Name of event ' + i + ' is not a string.'); }
+    if (!(event.date instanceof Date)) { throw new Error('Date of event ' + i + ' is not a Date object.'); }
+
+    var year = event.date.getFullYear();
+    var month = event.date.getMonth();
+
+    if (!(event.name in ymnTrees)) {
+      ymnTrees[event.name] = {};
+    }
+    if (!(year in ymnTrees[event.name])) {
+      ymnTrees[event.name][year] = {};
+    }
+    if (!(month in ymnTrees[event.name][year])) {
+      ymnTrees[event.name][year][month] = [];
+    }
+    ymnTrees[event.name][year][month].push(event);
   }
+  return ymnTrees;
+}
 
-  return events;
+function traverseYmnTree(subtree, parentNodeInfos) {
+  parentNodeInfos = parentNodeInfos || [];
+  if ('name' in subtree) {
+    var event = subtree;
+    addYmn(event, parentNodeInfos)
+  } else {
+    var siblings = Object.keys(subtree).length;
+    var index = 0;
+    for (var key in subtree) {
+      traverseYmnTree(subtree[key], parentNodeInfos.concat({
+        index: index++,
+        siblings: siblings
+      }));
+    }
+  }
+}
 
-};
+function addYmn(event, parentNodeInfos) {
+  event.ymn = [
+    !!(parentNodeInfos[2].siblings > 1 || parentNodeInfos[1].siblings > 1 || parentNodeInfos[0].siblings > 1),
+    !!(parentNodeInfos[2].siblings > 1 || parentNodeInfos[1].siblings > 1),
+    parentNodeInfos[2].siblings > 1 ? parentNodeInfos[2].index + 1 : 0
+  ];
+}
+
+module.exports = generateYmn;
--- test/index.js
+++ test/index.js
@@ -1,34 +1,147 @@
 var expect = require('chai').expect;
-var uniqueEventNames = require('../index');
+var generateYmn = require('../index');
 
-describe('uniqueEventNames', function() {
+describe('generateYmn', function() {
 
   it('should return array of same length', function(){
-    var events = [];
-    events = uniqueEventNames(events);
+    var events = generateYmn([]);
 
     expect(events).to.be.instanceof(Array);
     expect(events).to.have.length(0);
   });
 
   it('should throw error if event is not an object', function(){
-    var events = ['test'];
+    expect(function() {
 
-    expect(function() { uniqueEventNames(events); }).to.throw('Event 0 is not an object.');
+      generateYmn(['test']);
+
+    }).to.throw('Event 0 is not an object.');
   });
 
   it('should throw error if event name is not a string', function(){
-    var events = [{}];
+    expect(function() {
 
-    expect(function() { uniqueEventNames(events); }).to.throw('Name of event 0 is not a string.');
+      generateYmn([{}]);
+
+    }).to.throw('Name of event 0 is not a string.');
   });
 
   it('should throw error if event date is not a Date object', function(){
-    var events = [{
-      name: 'test'
-    }];
+    expect(function() {
 
-    expect(function() { uniqueEventNames(events); }).to.throw('Date of event 0 is not a Date object.');
+      generateYmn([{ name: 'test' }]);
+
+    }).to.throw('Date of event 0 is not a Date object.');
+  });
+
+  it('should generate ymn for one event correctly', function() {
+    var events = generateYmn([
+      {
+        name: 'test',
+        date: new Date(2014, 1, 1)
+      }
+    ]);
+    expect(events[0].ymn).to.eql([false, false, 0]);
+  });
+
+  it('should generate ymn for two events in different years correctly', function() {
+    var events = generateYmn([
+      {
+        name: 'test',
+        date: new Date(2014, 1, 1)
+      },
+      {
+        name: 'test',
+        date: new Date(2015, 1, 1)
+      }
+    ]);
+    expect(events[0].ymn).to.eql([true, false, 0]);
+    expect(events[1].ymn).to.eql([true, false, 0]);
+  });
+
+  it('should generate ymn for two events in the same year and different months correctly', function() {
+    var events = generateYmn([
+      {
+        name: 'test',
+        date: new Date(2015, 1, 1)
+      },
+      {
+        name: 'test',
+        date: new Date(2015, 2, 1)
+      }
+    ]);
+    expect(events[0].ymn).to.eql([true, true, 0]);
+    expect(events[1].ymn).to.eql([true, true, 0]);
+  });
+
+  it('should generate ymn for two events in the same year and same month correctly', function() {
+    var events = generateYmn([
+      {
+        name: 'test',
+        date: new Date(2015, 1, 1)
+      },
+      {
+        name: 'test',
+        date: new Date(2015, 1, 2)
+      }
+    ]);
+    expect(events[0].ymn).to.eql([true, true, 1]);
+    expect(events[1].ymn).to.eql([true, true, 2]);
+  });
+
+  it('should generate ymn for three events, two in same year, correctly', function() {
+    var events = generateYmn([
+      {
+        name: 'test',
+        date: new Date(2014, 1, 1)
+      },
+      {
+        name: 'test',
+        date: new Date(2015, 1, 2)
+      },
+      {
+        name: 'test',
+        date: new Date(2015, 2, 1)
+      }
+    ]);
+    expect(events[0].ymn).to.eql([true, false, 0]);
+    expect(events[1].ymn).to.eql([true, true, 0]);
+    expect(events[2].ymn).to.eql([true, true, 0]);
+  });
+
+  it('should generate ymn for three events, two in same month, correctly', function() {
+    var events = generateYmn([
+      {
+        name: 'test',
+        date: new Date(2015, 1, 1)
+      },
+      {
+        name: 'test',
+        date: new Date(2015, 2, 1)
+      },
+      {
+        name: 'test',
+        date: new Date(2015, 2, 2)
+      }
+    ]);
+    expect(events[0].ymn).to.eql([true, true, 0]);
+    expect(events[1].ymn).to.eql([true, true, 1]);
+    expect(events[2].ymn).to.eql([true, true, 2]);
+  });
+
+  it('should generate ymn for two different correctly', function() {
+    var events = generateYmn([
+      {
+        name: 'test1',
+        date: new Date(2015, 1, 1)
+      },
+      {
+        name: 'test2',
+        date: new Date(2015, 1, 1)
+      }
+    ]);
+    expect(events[0].ymn).to.eql([false, false, 0]);
+    expect(events[1].ymn).to.eql([false, false, 0]);
   });
 
 });
